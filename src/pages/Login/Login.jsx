5732d5be1fc6fed9e5a059579c51fafb2fb8b17f
Lucas Torres <lucasrib.ia@gmail.com>
2020-09-04 15:17:05 -0300

    Adding css reset

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import LoginText from '../../components/Login/LoginText'
 import LoginButton from '../../components/Login/LoginButton'
 
+import "./Login.css"
+
 export default props => {
 
     return (
