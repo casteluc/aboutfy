4ec54b7db2b5b5c0fe744a644872bdd69acd1ee9
Lucas Torres <lucasrib.ia@gmail.com>
2020-09-04 16:00:38 -0300

    Login page style

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter className="App">
+    <BrowserRouter>
       <Login />
     </BrowserRouter>
   );
--- src/components/Login/LoginButton.jsx
+++ src/components/Login/LoginButton.jsx
@@ -5,8 +5,8 @@ export default props => {
 
     return (
         <a className="login-button" href="http://localhost:8888/login">
-            <img src="" alt=""/>
-            <h3>Logar com o Spotfy</h3>
+            <img src={require('../../img/spotify-logo.svg')} alt=""/>
+            <h3>Logar com o Spotify</h3>
         </a>
     )
 }
