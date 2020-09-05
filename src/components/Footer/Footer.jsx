db739e0ad850b0b293b310f24ad70a3d13972fe7
Lucas Torres <lucasrib.ia@gmail.com>
2020-09-04 23:34:17 -0300

    Adding footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Footer.css'
+
+export default props => {
+
+    return (
+        <footer className="footer">
+            <p>&copy; Desenvolvido por <a href="https://www.instagram.com/casteluc/">@casteluc</a></p>
+        </footer>
+    )
+}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,13 +4,18 @@ import LoginText from '../../components/Login/LoginText'
 import LoginButton from '../../components/Login/LoginButton'
 
 import "./Login.css"
+import Footer from '../../components/Footer/Footer'
 
 export default props => {
 
     return (
         <div className="login">
-            <LoginText />
-            <LoginButton />
+            <div className="login-container">
+                <LoginText />
+                <LoginButton />
+            </div>
+
+            <Footer />
         </div>
     )
 }
