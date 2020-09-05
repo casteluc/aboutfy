82afaa2f30781e95e2301f6bd5c1081c777fdc4c
Lucas Torres <lucasrib.ia@gmail.com>
2020-09-04 23:55:52 -0300

    Getting user token and redirecting to another page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
-
+import queryString from 'query-string'
 import './App.css';
 import Login from './pages/Login/Login';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
+  const [accessToken, setAccessToken] = useState(false)
+  useEffect(() => {
+    let parsed = queryString.parse(window.location.search)
+    setAccessToken(parsed.access_token)
+  }, [])
+
   return (
-    <BrowserRouter>
-      <Login />
-    </BrowserRouter>
+    <div className="app">
+      {accessToken ?  <h1>Teste</h1> : <Login />}
+    </div>    
   );
 }
 
