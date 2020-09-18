981d10ec14a0ed08dc7ad02bbf81f85f988d8de2
Lucas Torres <lucasrib.ia@gmail.com>
2020-09-18 18:19:01 -0300

    Login link

--- src/components/Login/LoginButton/index.jsx
+++ src/components/Login/LoginButton/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default props => {
 
     return (
-        <a className="login-button" href="http://localhost:8888/login">
+        <a className="login-button" href="https://aboutfy-backend.herokuapp.com/login">
             <img src={require('../../../img/spotify-logo.svg')} alt=""/>
             <h3>Logar com o Spotify</h3>
         </a>
