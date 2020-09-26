47413a6f76d662b44aad5f74eb5239604ebad0e0
Lucas Torres <lucasrib.ia@gmail.com>
2020-09-25 21:21:42 -0300

    Adding styled components global patterns

---
+++ src/styles/patterns/Content.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export default styled.main`
+    max-width: 768px;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    flex-direction: column;  
+`
