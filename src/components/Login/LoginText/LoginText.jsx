9698a15947d5bca3b4db4acbd33cc9a626bc34ce
Lucas Torres <lucasrib.ia@gmail.com>
2020-09-25 21:35:50 -0300

    Adding styled components to LoginText

--- src/components/Login/LoginText/LoginText.jsx
+++ src/components/Login/LoginText/LoginText.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
+import { Container } from './LoginTextStyle'
+
 export default function LoginText() {
     return (
-        <div className="login-text">
+        <Container>
             <h2>Descubra o que você mais escutou durante a quarentena!</h2>
             <p>
                 Compartilhe com os seus amigos quais foram as suas músicas mais 
                 ouvidas durante esse período de isolamento e apoie os artistas que você admira.
             </p>
-        </div>
+        </Container>
     )
 }
---
+++ src/components/Login/LoginText/LoginTextStyle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    text-align: center;
+
+    h2 {
+        color: #fafafa;
+        font-size: 1.875rem;
+        margin-bottom: 20px;
+        font-weight: 700;
+    }
+`
